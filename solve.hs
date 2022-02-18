ca4f675a9502b9ec998b64226504a8380fa8ecf0
jonathansoufer <jonathanferreira23@gmail.com>
2022-02-18 15:56:05 +0000

    Add sudoku algorithms - Graham Hutton

---
+++ solve.hs
@@ -0,0 +1,76 @@
+-- This is the first tempt to solve a sudoku problem using haskell.
+-- Although this solves theoretically the problem, it is not the best solution.
+
+import Data.List
+
+-- Type declarations: The types below are used to represent the sudoku problem.
+
+-- A Grid is composed of a list of lists of Matrixes.
+type Grid = Matrix Value
+-- A Matrix is composed of a list of lists of Rows.
+type Matrix a = [Row a]
+-- A Row is composed of a list of lists of Values.
+type Row a = [a]
+-- Choices is a list of Chars.
+type Choices = [Value]
+-- A Value is any Char. Although this could be a integer, we use Char to make easy for printing solution.
+type Value = Char
+
+-- Haskell treat everything as functions. There's no variable in Haskell, but functions which return values
+
+-- Functions are created based on three steps:
+-- 1. Function declarations: function name :: function type
+-- 2. Function implementation: function name = function logic
+-- 3. Function call: function name params
+
+boxsize :: Int
+boxsize = 3
+
+values :: [Value]
+values = ['1'..'9']
+
+empty :: Value -> Bool
+empty = (== '.')
+
+single :: [a] -> Bool
+single [_] = True
+single _ = False
+
+rows :: Matrix a -> [Row a]
+rows = id
+
+cols :: Matrix a -> [Row a]
+cols = transpose
+
+boxs :: Matrix a -> [Row a]
+boxs =  unpack . map cols . pack   -- this is the same as unpack(map(cols(pack))) and is called functions composition
+        where
+            pack   = split . map split
+            split = chop boxsize
+            unpack = map concat . concat
+
+chop :: Int -> [a] -> [[a]]
+chop n [] = []
+chop n xs = take n xs : chop n (drop n xs)
+
+valid :: Grid -> Bool
+valid g = all nodups (rows g) && all nodups (cols g) && all nodups (boxs g)
+
+nodups :: Eq a => [a] -> Bool
+nodups [] = True
+nodups (x:xs) = notElem x xs && nodups xs
+
+choices :: Grid -> Matrix Choices
+choices g = map (map choice) (rows g)
+            where
+                choice v = if empty v then values else [v]
+
+cp :: [[a]] -> [[a]]
+cp [] =  [[]]
+cp (xs:xss) = [y:ys | y <- xs, ys <- cp xss]
+
+collapse :: Matrix [a] -> [Matrix a]
+collapse = cp . map cp
+
+solve :: Grid -> [Grid]
+solve =  filter valid . collapse . choices
